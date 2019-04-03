d3d695f8e90181cb8bbf0342f77ca21346bcc30b
ageca <anna.geca7@gmail.com>
2019-04-03 12:37:56 +0200

    lab8

---
+++ Lab8/dane.m
@@ -0,0 +1,17 @@
+LaiLfield = 1.766e-3; %H
+Laf = 6.008e-3; %H
+RaiRf = 0.12; %om
+B = 0.0002; %NMs
+J = 0.05; %kgm^2
+V = 0:48; %V
+i = 78; %A, max 250A
+wnom = 2800; %r/min, v=25 km/h
+
+m = 800; %kg
+A = 1.8; %m^2
+ro = 1.25; %kg/m^3
+C = 0.3;
+mirr = 0.015;
+r = 0.25; %m
+G = 11;
+g=9.81;
