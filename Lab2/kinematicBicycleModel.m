a619d2c23bcc960897d47ad8966effc05aa0ae49
baarrbaasia <b.kaczorowska1998@gmail.com>
2019-06-08 11:47:38 +0200

    rozwiązanie problemu + pdf

--- Lab2/kinematicBicycleModel.m
+++ Lab2/kinematicBicycleModel.m
@@ -1,8 +1,5 @@
 function pochodne = kinematicBicycleModel(~, x, u, optional_parameters)
-% x = [X Y psi v]
-% u = [delta_f delta_r a]
-% optional_parameters op = [lr lf]
-% pochodne = [dx, dy, dpsi, dv]
+% x = [X Y psi v], u = [delta_f delta_r a], optional_parameters op = [lr lf], pochodne = [dx, dy, dpsi, dv]
 beta = atan((optional_parameters(2)*tan(u(2))+optional_parameters(1)*tan(u(1)))/(optional_parameters(2)+optional_parameters(1)));
 dx = x(4)*cos(x(3)+beta); dy = x(4)*sin(x(3)+beta);
 dpsi = (x(4)*cos(beta))/(optional_parameters(1)+optional_parameters(2))*(tan(u(1))-tan(u(2)));
